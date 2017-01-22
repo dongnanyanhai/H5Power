6ebdd32e2c7c86a3a57cd88ca0eb988f633e6ffb
阿海 <dongnanyanhai@126.com>
2017-01-22 17:53:04 +0800

    增加动态调用微信js-sdk的接口；

--- extensions/function.php
+++ extensions/function.php
@@ -1512,3 +1512,15 @@ function goodluck($obj,$luck){
         }
     }
 }
+
+function getcurrenturl(){
+    $protocol = (!empty($_SERVER['HTTPS'])
+                    && $_SERVER['HTTPS'] !== 'off'
+                    || $_SERVER['SERVER_PORT'] === 443) ? 'https://' : 'http://';
+    if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
+        $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
+    } else {
+        $host = $_SERVER['HTTP_HOST'];
+    }
+    return $protocol.$host.$_SERVER['REQUEST_URI'];
+}
